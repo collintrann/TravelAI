52b24992ed604a0aff73ea5fbe4222efd6cd2b62
collintrann <uyytran@gmail.com>
2024-09-14 17:58:01 -0400

    embeddings

---
+++ backend/main.py
@@ -0,0 +1,22 @@
+from backend import uploadTikTok
+from langchain.prompts.chat import ChatPromptTemplate
+
+tiktok_urls = []
+
+for url in tiktok_urls:
+    transcription = uploadTikTok.upload_and_transcribe_tiktok(url)
+
+
+template = template = ChatPromptTemplate.from_messages(
+        [
+            ("system", 
+                """
+                You are a helpful Travel AI assistant expert in creating travel itineraries. Based on the user's saved
+                TikTok videos, you will help them create a travel itinerary for their next vacation.
+                """
+                ),
+                ("user", "{question} \n chat history: {chat_history} \n ai: "),
+        ]
+    )
+
+
