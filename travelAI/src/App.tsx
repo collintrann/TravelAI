03b1a6a6f6d348961bd49ca586a821a73695a1fe
Sydney <sydneydtran@gmail.com>
2024-09-14 22:52:04 -0400

    add inspo and carousel

--- travelAI/src/App.tsx
+++ travelAI/src/App.tsx
@@ -2,15 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import { GroupNameForm } from './pages/GroupNameForm';
 import { LocationForm } from './pages/LocationForm';
 import { Dashboard } from './pages/Dashboard';
+import { ThemeProvider } from './components/ui/theme-provider';
  
 const App = () => {
    return (
       <>
-         <Routes>
-            <Route path="/" element={<GroupNameForm />} />
-            <Route path="/location/:groupName" element={<LocationForm />} />
-            <Route path="/dashboard:groupName" element={<Dashboard />} />
-         </Routes>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <Routes>
+              <Route path="/" element={<GroupNameForm />} />
+              <Route path="/location/:groupName" element={<LocationForm />} />
+              <Route path="/dashboard/:groupName" element={<Dashboard />} />
+          </Routes>
+         </ThemeProvider>
       </>
    );
 };
--- travelAI/src/pages/Dashboard.tsx
+++ travelAI/src/pages/Dashboard.tsx
@@ -1,5 +1,49 @@
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+
+import { useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { useParams } from "react-router-dom";
+import { LinkForm } from "./LinkForm";
+import { TiktokCarousel } from "./TiktokCarousel";
+
 export function Dashboard() {
+    const { groupName } = useParams();
+
+    if (groupName === undefined) {
+        throw new Error("Group name not found");
+    }
+
+    const group = useQuery(api.groups.getGroup, {name: groupName});
+
     return (
-        <></>
+        <>
+            <Dialog>
+                <DialogTrigger asChild>
+                    <Button variant="outline">add inspo</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-md">
+                    <DialogTitle>add inspo</DialogTitle>
+                    <DialogDescription>
+                        copy & paste tiktok links here,
+                        paste one link at a time
+                    </DialogDescription>
+                    <LinkForm groupName={groupName}></LinkForm>
+            </DialogContent>
+            </Dialog>
+            <TiktokCarousel></TiktokCarousel>
+        </>
+        // <>
+        //     <p>{group?._id}</p>
+        //     <p>{group?.name}</p>
+        //     <p>{group?.location}</p>
+        //     <p>{group?.links}</p>
+        // </>
     )
 } 
